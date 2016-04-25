05122486e5d5d1418c554fce1351b834cd08e684
Ferruzo <kiaraferruzo@gmail.com>
2016-04-24 20:58:00 -0500

    Tipos de datos, variables y operadores

---
+++ tipos-de-datos-variables-y-operadores.js
@@ -0,0 +1,105 @@
+// Ejercicio 1
+
+/*
+var numero1 = prompt("Ingresa un número:");
+var numero2 = prompt("Ingresa otro número:");
+
+var cociente = numero1/numero2
+console.log(cociente)
+*/
+
+// Ejercicio 2
+
+/*
+var nombre = prompt("Ingresa tu nombre")
+var edad = prompt("Ingresa tu edad")
+
+var dias = edad*365
+
+alert(nombre + ', has vivido ' + dias + ' dias.')
+*/
+
+// Ejercicio 3
+
+/*
+var nombre = prompt("Ingresa tu nombre.")
+var primerApellido = prompt("Ingresa tu primer apellido.")
+var poblacion = prompt("¿Dónde vives?")
+
+alert('Hola, ' + nombre + ' ' + primerApellido + '\n' + 'Adiós, habitante de ' + poblacion)
+*/
+
+// Ejercicio 4
+
+/*
+var numero = parseInt(prompt("Ingresa un número."))
+var numero1 = numero + 5
+var numero2 = numero1 + 21
+var numero3 = numero2 - 4
+
+alert('Número introducido: ' + numero + '\n' + 'Contadores:     ' + numero1 + '     ' + numero2 + '     ' + numero3)
+*/
+
+// Ejercicio 5
+
+/*
+var u1 = parseInt(prompt("Ingresa el primero elemento del vector U."))
+var u2 = parseInt(prompt("Ingresa el segundo elemento del vector U."))
+
+var v1 = parseInt(prompt("Ingresa el primero elemento del vector V."))
+var v2 = parseInt(prompt("Ingresa el segundo elemento del vector V."))
+
+var producto = (u1*v1) + (u2*v2)
+
+alert('El prodcto escalar de los vectores:' + '\n(' + u1 + ',' + u2 + ') y (' + v1 + ',' + v2 + ') es ' + producto)
+*/
+
+// Ejercicio 6
+
+/*
+var x1 = parseInt(prompt("Ingresa el primero elemento del segmento."))
+var x2 = parseInt(prompt("Ingresa el segundo elemento del segmento."))
+
+var y1 = parseInt(prompt("Ingresa el primero elemento del segmento."))
+var y2 = parseInt(prompt("Ingresa el segundo elemento del segmento."))
+
+var M1 = x1 + x2
+var M2 = y1 + y2
+var R1 = M1 / 2
+var R2 = M2 / 2
+
+alert('El punto medio del segmento de extremos, los puntos:' + '\n' + '(' + x1 + ' ,' + x2 + ')' + ' y ' + '(' + y1 + ' ,' + y2 + ')' + ' es ' + '(' + R1 + ' ,' + R2 + ')')
+*/
+
+// Ejercicio 7
+
+/*
+var radio = parseInt(prompt("Ingresa el radio del círculo:"))
+var area = Math.PI*radio*radio
+
+alert("El área del círculo de radio " + radio + " es " + area)
+*/
+
+// Ejercicio 8
+
+/*
+var a,b
+a=parseInt(prompt("Escribe la base:"))
+b=parseInt(prompt("Escribe la altura:"))
+alert("Área = "+(a*b/2));
+*/
+
+// Ejercicio 9
+
+/*
+var a11 = prompt ("Ingresa el primer número de la matriz")
+var a12 = prompt ("Ingresa el segundo número de la matriz")
+var a21 = prompt ("Ingresa el tercer número de la matriz")
+var a22 = prompt ("Ingresa el cuarto número de la matriz")
+
+var resultado = a11*a22 - a12*a21 
+
+alert (resultado)
+*/
+
+// Ejercicio 10
